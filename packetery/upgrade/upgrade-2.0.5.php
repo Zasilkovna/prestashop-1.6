8d76a615f6faca08f7351398e37886cae9b1526c
Filip Jiskra <filip.jiskra@zasilkovna.cz>
2021-03-10 16:05:03 +0100

    PES-66: forcing removed

--- packetery/upgrade/upgrade-2.0.5.php
+++ packetery/upgrade/upgrade-2.0.5.php
@@ -6,7 +6,7 @@ if (!defined('_PS_VERSION_')) {
 
 function upgrade_module_2_0_5($object)
 {
-    return Db::getInstance()->execute('
+    $result = Db::getInstance()->execute('
         DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'packetery_carrier`;
         
         ALTER TABLE `' . _DB_PREFIX_ . 'packetery_address_delivery`
@@ -14,8 +14,12 @@ function upgrade_module_2_0_5($object)
         CHANGE `name_branch` `name_branch` varchar(255) NULL,
         CHANGE `currency_branch` `currency_branch` char(3) NULL,
         ADD `is_pickup_point` tinyint(1) NOT NULL;
-        
-        DELETE FROM `' . _DB_PREFIX_ . 'configuration` WHERE `name` = "PACKETERY_FORCED_COUNTRY";
-        DELETE FROM `' . _DB_PREFIX_ . 'configuration` WHERE `name` = "PACKETERY_FORCED_LANG";
     ');
+
+    if ($result) {
+        Configuration::deleteByName('PACKETERY_FORCED_COUNTRY');
+        Configuration::deleteByName('PACKETERY_FORCED_LANG');
+    }
+
+    return $result;
 }
