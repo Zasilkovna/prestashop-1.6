d74d97915eb4ebe86088747123928a40def6180d
Jan Potužník <jan.potuznik@packeta.com>
2021-03-11 16:34:35 +0100

    PES-70: pair carriers created by module during upgrade v2

--- packetery/upgrade/upgrade-2.0.5.php
+++ packetery/upgrade/upgrade-2.0.5.php
@@ -11,13 +11,28 @@ function upgrade_module_2_0_5($object)
         CHANGE `id_branch` `id_branch` int(11) NULL,
         CHANGE `name_branch` `name_branch` varchar(255) NULL,
         CHANGE `currency_branch` `currency_branch` char(3) NULL,
-        ADD `is_pickup_point` tinyint(1) NOT NULL;
-        
-        INSERT INTO `' . _DB_PREFIX_ . 'packetery_address_delivery` (`id_carrier`, `is_cod`, `is_pickup_point`)
-        SELECT `id_carrier`, `is_cod`, 1 FROM `' . _DB_PREFIX_ . 'packetery_carrier`;
-
-        DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'packetery_carrier`;
+        ADD `is_pickup_point` tinyint(1) NOT NULL
     ');
+    if ($result === false) {
+        return $result;
+    }
+
+    $oldPacketeryCarriers = Db::getInstance()->executeS('SELECT * FROM `' . _DB_PREFIX_ . 'packetery_carrier`');
+    if ($oldPacketeryCarriers) {
+        $psCarriers = Db::getInstance()->executeS('SELECT `id_carrier` FROM `' . _DB_PREFIX_ . 'carrier` WHERE `deleted` = 0');
+        $psCarriers = array_column($psCarriers, 'id_carrier');
+        $carriersToPair = [];
+        foreach ($oldPacketeryCarriers as $oldPacketeryCarrier) {
+            if (in_array($oldPacketeryCarrier['id_carrier'], $psCarriers)) {
+                $carriersToPair[] = ['id_carrier' => $oldPacketeryCarrier['id_carrier'], 'is_cod' => $oldPacketeryCarrier['is_cod'], 'is_pickup_point' => 1];
+            }
+        }
+    }
+    if ($carriersToPair) {
+        Db::getInstance()->insert('packetery_address_delivery', $carriersToPair);
+    }
+
+    $result = ('DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'packetery_carrier`');
 
     if ($result) {
         Configuration::deleteByName('PACKETERY_FORCED_COUNTRY');
