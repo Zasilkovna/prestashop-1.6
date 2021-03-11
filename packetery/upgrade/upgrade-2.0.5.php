64ce559634f869808b92830ea0d9994546cb79e7
Jan Potužník <jan.potuznik@packeta.com>
2021-03-11 14:39:15 +0100

    PES-70: pair carriers created by module during upgrade

--- packetery/upgrade/upgrade-2.0.5.php
+++ packetery/upgrade/upgrade-2.0.5.php
@@ -7,13 +7,16 @@ if (!defined('_PS_VERSION_')) {
 function upgrade_module_2_0_5($object)
 {
     $result = Db::getInstance()->execute('
-        DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'packetery_carrier`;
-        
         ALTER TABLE `' . _DB_PREFIX_ . 'packetery_address_delivery`
         CHANGE `id_branch` `id_branch` int(11) NULL,
         CHANGE `name_branch` `name_branch` varchar(255) NULL,
         CHANGE `currency_branch` `currency_branch` char(3) NULL,
         ADD `is_pickup_point` tinyint(1) NOT NULL;
+        
+        INSERT INTO `' . _DB_PREFIX_ . 'packetery_address_delivery` (`id_carrier`, `is_cod`, `is_pickup_point`)
+        SELECT `id_carrier`, `is_cod`, 1 FROM `' . _DB_PREFIX_ . 'packetery_carrier`;
+
+        DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'packetery_carrier`;
     ');
 
     if ($result) {
