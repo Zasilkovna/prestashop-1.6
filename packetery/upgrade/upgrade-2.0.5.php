fe17283add36b8ddd4e3fb1bd38d4676086754d8
jan-potuznik-z <79142524+jan-potuznik-z@users.noreply.github.com>
2021-03-12 12:08:07 +0100

    PES-70: pair carriers created by module during upgrade (#6)

--- packetery/upgrade/upgrade-2.0.5.php
+++ packetery/upgrade/upgrade-2.0.5.php
@@ -7,14 +7,38 @@ if (!defined('_PS_VERSION_')) {
 function upgrade_module_2_0_5($object)
 {
     $result = Db::getInstance()->execute('
-        DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'packetery_carrier`;
-        
         ALTER TABLE `' . _DB_PREFIX_ . 'packetery_address_delivery`
         CHANGE `id_branch` `id_branch` int(11) NULL,
         CHANGE `name_branch` `name_branch` varchar(255) NULL,
         CHANGE `currency_branch` `currency_branch` char(3) NULL,
-        ADD `is_pickup_point` tinyint(1) NOT NULL;
+        ADD `is_pickup_point` tinyint(1) NOT NULL
     ');
+    if ($result === false) {
+        return $result;
+    }
+
+    $carriersToPair = [];
+    $oldPacketeryCarriers = Db::getInstance()->executeS('
+        SELECT `id_carrier`, `is_cod` FROM `' . _DB_PREFIX_ . 'packetery_carrier`');
+    if ($oldPacketeryCarriers) {
+        $psCarriers = Carrier::getCarriers(Configuration::get('PS_LANG_DEFAULT'), false, false, false, null,
+            Carrier::PS_CARRIERS_AND_CARRIER_MODULES_NEED_RANGE);
+        $psCarriersIds = array_column($psCarriers, 'id_carrier');
+        foreach ($oldPacketeryCarriers as $oldPacketeryCarrier) {
+            if (in_array($oldPacketeryCarrier['id_carrier'], $psCarriersIds)) {
+                $carriersToPair[] = [
+                    'id_carrier' => $oldPacketeryCarrier['id_carrier'],
+                    'is_cod' => $oldPacketeryCarrier['is_cod'],
+                    'is_pickup_point' => 1,
+                ];
+            }
+        }
+    }
+    if ($carriersToPair) {
+        Db::getInstance()->insert('packetery_address_delivery', $carriersToPair);
+    }
+
+    $result = Db::getInstance()->execute('DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'packetery_carrier`');
 
     if ($result) {
         Configuration::deleteByName('PACKETERY_FORCED_COUNTRY');
